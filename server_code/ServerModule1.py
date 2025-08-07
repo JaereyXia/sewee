4d51328ae2dd11e36e77f164b8bf6eb82bc7410a
jaerey1016@gmail.com <jaerey1016@gmail.com>
2025-08-07 02:35:28 +0000

    Created image-Photoroom.png and ServerModule1; Edited settings and Form1

---
+++ server_code/ServerModule1.py
@@ -0,0 +1,15 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+from datetime import datetime
+
+#Store feedback data using a server function
+@anvil.server.callable
+def add_feedback(name, email, feedback):
+  app_tables.feedback.add_row(
+    name=name, 
+    email=email, 
+    feedback=feedback, 
+    created=datetime.now()
+  )
