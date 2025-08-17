8d094981efc0d6581acf9f9814898fb954125049
jaerey1016@gmail.com <jaerey1016@gmail.com>
2025-08-17 21:35:02 +0000

    Created loading_page and sewee_logo.png; Edited settings and home_page

--- client_code/loading_page/__init__.py
+++ client_code/loading_page/__init__.py
@@ -1,17 +1,17 @@
-from ._anvil_designer import home_pageTemplate
+from ._anvil_designer import loading_pageTemplate
 from anvil import *
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class home_page(home_pageTemplate):
+
+class loading_page(loading_pageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  def add_clock_botton_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    open_form('clock_page')
+c = Timer(interval=0.1)
+if c == 1:
+  open_form('home_page')
