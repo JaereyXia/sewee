23c7208d536847b9538440c245da063b2e6a598e
jaerey1016@gmail.com <jaerey1016@gmail.com>
2025-08-18 03:06:40 +0000

    Edited clock_page

--- client_code/clock_page/__init__.py
+++ client_code/clock_page/__init__.py
@@ -12,3 +12,7 @@ class clock_page(clock_pageTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.role()
