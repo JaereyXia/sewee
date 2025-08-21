8de26cfe46676081985f88cbb9a53d94ce24c451
jaerey1016@gmail.com <jaerey1016@gmail.com>
2025-08-21 03:05:25 +0000

    Edited clock_page

--- client_code/clock_page/__init__.py
+++ client_code/clock_page/__init__.py
@@ -20,7 +20,7 @@ class clock_page(clock_pageTemplate):
     try:
       if float(int(self.time_number_hour_box.text)) >12 or float(int(self.time_number_hour_box.text)) < 0:
           self.time_number_hour_box.text = ""
-          Notification("Wrong number, please inter times within 12 hour").show()
+          Notification("Wrong number, please enter a time within 12 hours").show()
     except ValueError:
       Notification("Whoops! This wasn't a whole number").show()
       self.time_number_hour_box.text = ""
@@ -28,9 +28,9 @@ class clock_page(clock_pageTemplate):
      
   def time_number_minute_box_pressed_enter(self, **event_args):
     try:
-      if int(self.time_number_minute_box.text) >60 or int(self.time_number_minute_box.text) < 0:
+      if int(self.time_number_minute_box.text) >59 or int(self.time_number_minute_box.text) < 0:
         self.time_number_minute_box.text = ""
-        Notification("Wrong number, please inter times within 60 minute").show()
+        Notification("Wrong number, please enter a value of less than 60 minutes").show()
     except ValueError:
       Notification("Whoops! This wasn't a whole number").show()
       self.time_number_minute_box.text = ""
@@ -95,16 +95,24 @@ class clock_page(clock_pageTemplate):
 
   def write_cycle_number_box_pressed_enter(self, **event_args):
     try:
-      if int(self.write_cycle_number_box.text) >60 or int(self.write_cycle_number_box.text) < 0:
+      if int(self.write_cycle_number_box.text) >20 or int(self.write_cycle_number_box.text) < 0:
         self.write_cycle_number_box.text = ""
-        Notification("Wrong number, please inter times within 60 minute").show()
+        Notification("Wrong number, please enter a number less than 20").show()
     except ValueError:
       Notification("Whoops! This wasn't a whole number").show()
       self.write_cycle_number_box.text = ""
 
   def alarm_interval_number_box_pressed_enter(self, **event_args):
-    """This method is called when the user presses Enter in this text box"""
-    pass
+    try:
+      if int(self.alarm_interval_number_box.text) >10 or int(self.alarm_interval_number_box.text) < 0:
+        self.alarm_interval_number_box.text = ""
+        Notification("Wrong number, please enter a number less than 10").show()
+    except ValueError:
+      Notification("Whoops! This wasn't a whole number").show()
+      self.alarm_interval_number_box.text = ""
+    
+
+
 
 
 
