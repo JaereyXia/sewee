5bf3c59f7d84034070246e8741e13962d4bbe746
jaerey1016@gmail.com <jaerey1016@gmail.com>
2025-08-26 23:23:38 +0000

    Edited clock_page

--- client_code/clock_page/__init__.py
+++ client_code/clock_page/__init__.py
@@ -25,7 +25,6 @@ class clock_page(clock_pageTemplate):
       Notification("Whoops! This wasn't a whole number").show()
       self.time_number_hour_box.text = ""
     
-     
   def time_number_minute_box_pressed_enter(self, **event_args):#If the user enter a wrong time, it will detect and tell the user
     try:#if the time was over 59 minute that means it's 1 hour, or if the time is less then 1 minute, the app will tell the user 
       if int(self.time_number_minute_box.text) >59 or int(self.time_number_minute_box.text) < 0:
@@ -53,55 +52,55 @@ class clock_page(clock_pageTemplate):
       Notification("Whoops! This wasn't a whole number").show()
       self.alarm_interval_number_box.text = ""
 
-  def time_bottn_am_click(self, **event_args):
+  def time_bottn_am_click(self, **event_args):#time am
     if self.time_button_am.role == 'elevated-button':
       self.time_button_am.role= 'filled-button'
     else:
       self.time_button_am.role = 'elevated-button'
 
-  def time_pm_button_click(self, **event_args):
+  def time_pm_button_click(self, **event_args):#time pm
     if self.time_button_pm.role == 'elevated-button':
       self.time_button_pm.role= 'filled-button'
     else:
       self.time_button_pm.role = 'elevated-button'
 
-  def days_mon_button_click(self, **event_args):
+  def days_mon_button_click(self, **event_args):#weekend
     if self.days_mon_button.role == 'elevated-button':
       self.days_mon_button.role= 'filled-button'
     else:
       self.days_mon_button.role = 'elevated-button'
 
-  def days_tue_button_click(self, **event_args):
+  def days_tue_button_click(self, **event_args):#weekend
     if self.days_tue_button.role == 'elevated-button':
       self.days_tue_button.role= 'filled-button'
     else:
       self.days_tue_button.role = 'elevated-button'
 
-  def days_wed_button_click(self, **event_args):
+  def days_wed_button_click(self, **event_args):#weekend
     if self.days_wed_button.role == 'elevated-button':
       self.days_wed_button.role= 'filled-button'
     else:
       self.days_wed_button.role = 'elevated-button'
 
-  def days_thu_button_click(self, **event_args):
+  def days_thu_button_click(self, **event_args):#weekend
     if self.days_thu_button.role == 'elevated-button':
       self.days_thu_button.role= 'filled-button'
     else:
       self.days_thu_button.role = 'elevated-button'
 
-  def days_fri_button_click(self, **event_args):
+  def days_fri_button_click(self, **event_args):#weekend
     if self.days_fri_button.role == 'elevated-button':
       self.days_fri_button.role= 'filled-button'
     else:
       self.days_fri_button.role = 'elevated-button'
 
-  def days_sat_button_click(self, **event_args):
+  def days_sat_button_click(self, **event_args):#weekend
     if self.days_sat_button.role == 'elevated-button':
       self.days_sat_button.role= 'filled-button'
     else:
       self.days_sat_button.role = 'elevated-button'
 
-  def days_sun_button_click(self, **event_args):
+  def days_sun_button_click(self, **event_args):#weekend
     if self.days_sun_button.role == 'elevated-button':
       self.days_sun_button.role= 'filled-button'
     else:
