f1336a672df5ab0873fd829d5fcac87253e18f4c
jaerey1016@gmail.com <jaerey1016@gmail.com>
2025-08-28 02:12:32 +0000

    Edited ServerModule1, clock_page, feedback_form, home_page and 2 more

--- client_code/loading_page/__init__.py
+++ client_code/loading_page/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import loading_pageTemplate
 from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
@@ -10,16 +13,17 @@ from anvil.tables import app_tables
 class loading_page(loading_pageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-    
-
+    self.init_components(**properties)  
     # Any code you write here will run before the form opens.
+    anvil.users.login_with_form()
 
   def timer_1_tick(self, **event_args):#after 5 sec, the user will open the app.
     open_form('home_page')
 
   def button_1_click(self, **event_args):#if the user stack on the loading screen, they can report the question to feedback form
     open_form('feedback_form')
+
+  
     
   
 
