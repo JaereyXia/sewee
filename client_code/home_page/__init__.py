0b7c5c237fe65721d1c868385698453b9e2e906f
jaerey1016@gmail.com <jaerey1016@gmail.com>
2025-08-31 21:31:14 +0000

    Edited settings and home_page

--- client_code/home_page/__init__.py
+++ client_code/home_page/__init__.py
@@ -8,18 +8,29 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
+
 class home_page(home_pageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    
+
+    world_label = Label(text="World")
+    lp.add_component(world_label)
+
+    hello_label = Label(text="Hello")
+    #add the hello_label as the first component in the Linear Panel
+    lp.add_component(hello_label, index=0)
+
+    
+
 
     # Any code you write here will run before the form opens.
-
-  
-  
   
   def add_clock_botton_click(self, **event_args):#open clock opage
     open_form('clock_page')
   
   def feedback_button_click(self, **event_args):#open feedback form 
     open_form('feedback_form')
+
+
