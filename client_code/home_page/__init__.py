104a7b3402b4d4e330c7bf358b748be2674ad931
jaerey1016@gmail.com <jaerey1016@gmail.com>
2025-09-01 00:00:09 +0000

    Edited home_page

--- client_code/home_page/__init__.py
+++ client_code/home_page/__init__.py
@@ -13,24 +13,22 @@ class home_page(home_pageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    
-
-    world_label = Label(text="World")
-    lp.add_component(world_label)
-
-    hello_label = Label(text="Hello")
-    #add the hello_label as the first component in the Linear Panel
-    lp.add_component(hello_label, index=0)
-
-    
-
 
     # Any code you write here will run before the form opens.
+    self.add_clock()#load all the clock
+    
+  
+
+  def add_clock(self):#this add all the clock with the information saved form data server
+    clock = Label(text="Hello")#save all the infor of the clock from data.
+    self.Clocks.add_component(clock, index=0)#add the clock
+    
   
   def add_clock_botton_click(self, **event_args):#open clock opage
     open_form('clock_page')
   
   def feedback_button_click(self, **event_args):#open feedback form 
     open_form('feedback_form')
+
 
 
