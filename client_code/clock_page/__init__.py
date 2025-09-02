0185a299496603f42c8f02450b5a0f47b31dd174
jaerey1016@gmail.com <jaerey1016@gmail.com>
2025-09-02 23:53:52 +0000

    Edited settings and clock_page

--- client_code/clock_page/__init__.py
+++ client_code/clock_page/__init__.py
@@ -36,6 +36,14 @@ class clock_page(clock_pageTemplate):
     except ValueError:
       Notification("Whoops! This wasn't a whole number").show()
       self.time_number_hour_box.text = ""
+  def time_number_hour_box_lost_focus(self, **event_args):
+    try:#If the time was over 12hour or less then 1 hour, the app will send a notification telling the user
+      if float(int(self.time_number_hour_box.text)) >12 or float(int(self.time_number_hour_box.text)) < 0:
+        self.time_number_hour_box.text = ""
+        Notification("Wrong number, please enter a time within 12 hours").show()
+    except ValueError:
+      Notification("Whoops! This wasn't a whole number").show()
+      self.time_number_hour_box.text = ""
     
   def time_number_minute_box_pressed_enter(self, **event_args):#If the user enter a wrong time, it will detect and tell the user
     try:#if the time was over 59 minute that means it's 1 hour, or if the time is less then 1 minute, the app will tell the user 
@@ -45,7 +53,15 @@ class clock_page(clock_pageTemplate):
     except ValueError:
       Notification("Whoops! This wasn't a whole number").show()
       self.time_number_minute_box.text = ""
-
+  def time_number_minute_box_lost_focus(self, **event_args):
+    try:#if the time was over 59 minute that means it's 1 hour, or if the time is less then 1 minute, the app will tell the user 
+      if int(self.time_number_minute_box.text) >59 or int(self.time_number_minute_box.text) < 0:
+        self.time_number_minute_box.text = ""
+        Notification("Wrong number, please enter a value of less than 60 minutes").show()
+    except ValueError:
+      Notification("Whoops! This wasn't a whole number").show()
+      self.time_number_minute_box.text = ""
+      
   def write_cycle_number_box_pressed_enter(self, **event_args):#If the user enter a wrong number, it will detect and tell the user
     try:#if the number was over the 20 maximum, or if the time is less then 1 minimum, the app will tell the user 
       if int(self.write_cycle_number_box.text) >20 or int(self.write_cycle_number_box.text) < 0:
@@ -54,7 +70,15 @@ class clock_page(clock_pageTemplate):
     except ValueError:
       Notification("Whoops! This wasn't a whole number").show()
       self.write_cycle_number_box.text = ""
-
+  def write_cycle_number_box_lost_focus(self, **event_args):
+    try:#if the number was over the 20 maximum, or if the time is less then 1 minimum, the app will tell the user 
+      if int(self.write_cycle_number_box.text) >20 or int(self.write_cycle_number_box.text) < 0:
+        self.write_cycle_number_box.text = ""
+        Notification("Wrong number, please enter a number less than 20").show()
+    except ValueError:
+      Notification("Whoops! This wasn't a whole number").show()
+      self.write_cycle_number_box.text = ""
+  
   def alarm_interval_number_box_pressed_enter(self, **event_args):#If the user enter a wrong number, it will detect and tell the user
     try:#if the time was over 10 minute the maximum, or if the time is less then 1 minute the mimimum, the app will tell the user 
       if int(self.alarm_interval_number_box.text) >10 or int(self.alarm_interval_number_box.text) < 0:
@@ -63,7 +87,15 @@ class clock_page(clock_pageTemplate):
     except ValueError:
       Notification("Whoops! This wasn't a whole number").show()
       self.alarm_interval_number_box.text = ""
-
+  def alarm_interval_number_box_lost_focus(self, **event_args):
+    try:#if the time was over 10 minute the maximum, or if the time is less then 1 minute the mimimum, the app will tell the user 
+      if int(self.alarm_interval_number_box.text) >10 or int(self.alarm_interval_number_box.text) < 0:
+        self.alarm_interval_number_box.text = ""
+        Notification("Wrong number, please enter a number less than 10").show()
+    except ValueError:
+      Notification("Whoops! This wasn't a whole number").show()
+      self.alarm_interval_number_box.text = ""
+  
   def time_bottn_am_click(self, **event_args):#time am
     if self.time_button_am.role == 'elevated-button':
       self.time_button_am.role= 'filled-button'
@@ -187,14 +219,12 @@ class clock_page(clock_pageTemplate):
     else:
       pass
 
-  def time_number_hour_box_lost_focus(self, **event_args):
-    try:#If the time was over 12hour or less then 1 hour, the app will send a notification telling the user
-      if float(int(self.time_number_hour_box.text)) >12 or float(int(self.time_number_hour_box.text)) < 0:
-        self.time_number_hour_box.text = ""
-        Notification("Wrong number, please enter a time within 12 hours").show()
-    except ValueError:
-      Notification("Whoops! This wasn't a whole number").show()
-      self.time_number_hour_box.text = ""
+
+
+
+
+
+
 
     
     
